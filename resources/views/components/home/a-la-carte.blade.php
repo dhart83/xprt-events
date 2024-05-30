7d3b95fcb1789278c4d4ffaaabd0014543b2ca36
Donnie Hartman <hartman_donnie@hotmail.com>
2024-05-30 19:46:26 -0400

    :memo: Update booking steps and links.

--- resources/views/components/home/a-la-carte.blade.php
+++ resources/views/components/home/a-la-carte.blade.php
@@ -8,82 +8,88 @@
             description="Mix and match to create a personalized adventure that perfectly suits your event's unique style">
             Create your custom experience in 3 easy steps</x-banner.heading>
 
-        <x-step-segment step="1" title="Explore our base package">
-            <x-slot name="left">
-                <p>Begin your photobooth journey by reviewing our comprehensive base package. Discover all the exciting
-                    features and
-                    inclusions that form the foundation of your memorable event.</p>
-            </x-slot>
-            <x-slot name="right">
-                <a href="">
-                    <div class="relative w-52 h-80 hover:scale-105 transition ease-in-out delay-[50ms] duration-300">
-                        <img class="absolute object-cover w-full h-full" src={{
-                            asset('assets/images/salsa/salsa-head.svg') }} alt="">
-                        <img class="absolute object-cover w-full h-full -translate-x-1 -translate-y-4 scale-105"
-                            type="image/svg+xml" src={{ asset('assets/images/salsa/salsa-led-ring.svg') }} alt="">
-                        <video class="absolute object-cover h-[125px] translate-x-[53px] translate-y-[80px]" autoplay
-                            loop muted>
-                            <source src={{ asset('assets/images/salsa/salsa-video.m4v') }} type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                </a>
-            </x-slot>
-            <x-slot name="cta">
-                <p>Learn more about our
-                    <a href="/"
-                        class="text-secondary underline underline-offset-2 hover:text-primary transition ease-in-out delay-[50ms] duration-300">base
-                        package</a>
-                </p>
-            </x-slot>
-        </x-step-segment>
-
-
-        <x-step-segment step="2" title="Tailor Your Experience">
-            <x-slot name="left">
-                <p>
-                    Take your event to the next level by customizing your photobooth experience with our wide range of
-                    add-ons. Whether it's
-                    extra hours, themed props, or unique backdrops, our add-ons allow you to personalize your photobooth
-                    rental to suit your
-                    event's unique style and needs.
-                </p>
-            </x-slot>
-            <x-slot name="cta">
-                <p>Explore our
-                    <a href="/"
-                        class="text-secondary underline underline-offset-2 hover:text-primary transition ease-in-out delay-[50ms] duration-300">customizations</a>
-                </p>
-            </x-slot>
-        </x-step-segment>
-
-
-        <x-step-segment step="3" title="Book Your Event">
-            <x-slot name="left">
-                <p>
-                    Ready to make your event unforgettable? It's time to book your date with us! Get in touch with our
-                    friendly
-                    team to
-                    discuss your event details, check availability, and secure your photobooth rental.
-                </p>
-            </x-slot>
-            <x-slot name="right">
-                <div
-                    class="rounded-lg overflow-hidden shadow-lg dark:shadow-black hover:shadow-xl transition ease-in-out delay-[50ms] duration-300">
-                    <a href="{{ route('contact.index') }}">
-                        <img class="block dark:hidden object-cover w-full h-full" src={{
-                            asset('assets/images/contact-form.JPG') }} alt="">
-                        <img class="hidden dark:block object-cover w-full h-full" src={{
-                            asset('assets/images/contact-form-dark.JPG') }} alt="">
+        <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+            <x-step-segment step="1" title="Choose a Base Package">
+                <x-slot name="left">
+                    <p>Begin your photobooth journey by exploring our comprehensive base package. Discover all the
+                        exciting features and
+                        inclusions that form the foundation of your unforgettable event.</p>
+                </x-slot>
+                {{-- <x-slot name="right">
+                    <a href="">
+                        <div
+                            class="relative w-52 h-80 hover:scale-105 transition ease-in-out delay-[50ms] duration-300">
+                            <img class="absolute object-cover w-full h-full" src={{
+                                asset('assets/images/salsa/salsa-head.svg') }} alt="">
+                            <img class="absolute object-cover w-full h-full -translate-x-1 -translate-y-4 scale-105"
+                                type="image/svg+xml" src={{ asset('assets/images/salsa/salsa-led-ring.svg') }} alt="">
+                            <video class="absolute object-cover h-[125px] translate-x-[53px] translate-y-[80px]"
+                                autoplay loop muted>
+                                <source src={{ asset('assets/images/salsa/salsa-video.m4v') }} type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
                     </a>
-                </div>
-            </x-slot>
-            <x-slot name="cta">
-                <div class="mt-6">
-                    <a class="btn btn-primary" href="{{ route('contact.index') }}">Book Now</a>
-                </div>
-            </x-slot>
-        </x-step-segment>
+                </x-slot> --}}
+                <x-slot name="cta">
+                    <p>Learn more about our
+                        <a href="#packages"
+                            class="text-secondary underline underline-offset-2 hover:text-primary transition ease-in-out delay-[50ms] duration-300">base
+                            packages</a>
+                    </p>
+                </x-slot>
+            </x-step-segment>
+
+
+            <x-step-segment step="2" title="Customize Your Experience">
+                <x-slot name="left">
+                    <p>
+                        Take your event to the next level by customizing your photobooth experience with our wide range
+                        of
+                        add-ons. Whether it's
+                        extra hours, themed props, or unique backdrops, our add-ons allow you to personalize your
+                        photobooth
+                        rental to suit your
+                        event's unique style and needs.
+                    </p>
+                </x-slot>
+                <x-slot name="cta">
+                    <p>Ask us about our
+                        <a href="{{ route('contact.index') }}"
+                            class="text-secondary underline underline-offset-2 hover:text-primary transition ease-in-out delay-[50ms] duration-300">customizations</a>
+                    </p>
+                </x-slot>
+            </x-step-segment>
+
+
+            <x-step-segment step="3" title="Book Your Date">
+                <x-slot name="left">
+                    <p>
+                        Ready to make your event unforgettable? It's time to book your date with us! Get in touch with
+                        our
+                        friendly
+                        team to
+                        discuss your event details, check availability, and secure your photobooth rental.
+                    </p>
+                </x-slot>
+                {{-- <x-slot name="right">
+                    <div
+                        class="rounded-lg overflow-hidden shadow-lg dark:shadow-black hover:shadow-xl transition ease-in-out delay-[50ms] duration-300">
+                        <a href="{{ route('contact.index') }}">
+                            <img class="block dark:hidden object-cover w-full h-full" src={{
+                                asset('assets/images/contact-form.JPG') }} alt="">
+                            <img class="hidden dark:block object-cover w-full h-full" src={{
+                                asset('assets/images/contact-form-dark.JPG') }} alt="">
+                        </a>
+                    </div>
+                </x-slot> --}}
+                <x-slot name="cta">
+                    <div class="mt-6">
+                        <a class="btn btn-primary" href="{{ route('contact.index') }}">Book Now</a>
+                    </div>
+                </x-slot>
+            </x-step-segment>
+        </div>
 
 
     </x-layout.container>
--- resources/views/components/step-segment.blade.php
+++ resources/views/components/step-segment.blade.php
@@ -1,31 +1,36 @@
 <div {{ $attributes->merge([
     'class' =>
-    'step relative grid lg:grid-cols-2 gap-16 max-w-6xl pt-16 pb-24 md:p-16 md:pb-24 mx-auto border-b md:border-b-0
+    'step relative grid lg:grid-cols-1 gap-16 max-w-6xl py-16 md:p-16 mx-auto border-b md:border-b-0
     md:border-l border-gray-400'
     ])}}>
-    <div class="flex flex-col justify-between gap-6">
-        <h3 class="font-sans font-bold text-[22px] m-0">
-            <span class="bg-clip-text text-transparent bg-gradient-to-r from-secondary to-primary">
-                Step. {{ $step }}
-            </span>
-        </h3>
-        <h4 class="text-3xl md:text-[40px] font-sans font-semibold m-0 leading-[3rem]">{{ $title }}</h4>
-        <div>
-            @if (isset($left))
-            {{ $left }}
-            @else
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptas rem, explicabo
-                temporibus eveniet illo sint voluptates unde animi expedita delectus eligendi, earum
-                tenetur! Quis suscipit praesentium quae nobis placeat.</p>
-            @endif
+    {{-- <div class="flex flex-col justify-between gap-3"> --}}
+        <div class="flex flex-col justify-between">
+            <div class="mb-6">
+                <h3 class="font-sans font-bold text-[22px] m-0">
+                    <span class="bg-clip-text text-transparent bg-gradient-to-r from-secondary to-primary">
+                        Step. {{ $step }}
+                    </span>
+                </h3>
+                <h4 class="text-2xl mt-6 mb-8 md:text-4xl font-sans font-semibold m-0 leading-[3rem]">{{ $title }}</h4>
+                <div>
+                    @if (isset($left))
+                    {{ $left }}
+                    @else
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptas rem, explicabo
+                        temporibus eveniet illo sint voluptates unde animi expedita delectus eligendi, earum
+                        tenetur! Quis suscipit praesentium quae nobis placeat.</p>
+                    @endif
+                </div>
+            </div>
+            <div>
+                {{ $cta }}
+            </div>
         </div>
-        {{ $cta }}
+        {{-- <div class="flex justify-center items-center">
+            @if (isset($right))
+            {{ $right }}
+            @else
+            <img src={{ asset('assets/images/no-image.svg') }} alt="">
+            @endif
+        </div> --}}
     </div>
-    <div class="flex justify-center items-center">
-        @if (isset($right))
-        {{ $right }}
-        @else
-        <img src={{ asset('assets/images/no-image.svg') }} alt="">
-        @endif
-    </div>
-</div>
