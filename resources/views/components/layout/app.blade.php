cbd33b546318782ef87d04206d9fa33d9b3a5c23
Donnie Hartman <hartman_donnie@hotmail.com>
2026-03-05 22:37:13 -0500

    refactor(layout): clean app.blade.php and align with new branding styles
    - remove commented-out code
    - update legacy brand classes to use new design tokens
    - ensure layout styles match updated branding system

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -5,9 +5,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{--
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> --}}
-
 
         <meta name="description"
             content="XPRT Events LLC: Elevate your events with our state-of-the-art photobooth experiences. Weddings, corporate gatherings, birthdays, and more. Unforgettable memories, innovative technology">
@@ -16,32 +13,21 @@
         <meta name="author" content="Donnie H.">
         <meta name="robots" content="index, follow">
 
-        {{--
-        <link rel="icon" href="favicon.ico" type="image/x-icon"> TODO: Update icon image --}}
-
+    
         <meta property="og:title" content="XPRT Events LLC - Capturing Memorable Moments">
         <meta property="og:description"
             content="XPRT Events LLC is your trusted partner in capturing memorable moments at your special events. We offer top-tier photo booth services and more. Contact us today!" />
-        {{--
-        <meta property="og:image" content="image_url"> --}}
+
         <meta property="og:url" content="https://xprtevents.com">
 
         <meta name="twitter:card" content="summary_large_image">
-        {{--
-        <meta name="twitter:site" content="@your_twitter_handle"> --}}
+
         <meta name="twitter:title" content="XPRT Events LLC - Capturing Memorable Moments">
         <meta name="twitter:description"
             content="XPRT Events LLC is your trusted partner in capturing memorable moments at your special events. We offer top-tier photo booth services and more. Contact us today!" />
-        {{--
-        <meta name="twitter:image" content="image_url"> --}}
+   
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        {{--
-        <link rel="canonical" href="preferred_url"> --}}
-
-
-
 
 
         <title>{{ config('app.name', 'XPRT Events LLC') }}</title>
@@ -60,10 +46,8 @@
 
     </head>
 
-    <body {{--
-        class="font-sans antialiased flex flex-col min-h-screen bg-white text-gray-950 dark:bg-gray-950 dark:text-gray-200">
-        --}}
-        class="font-sans antialiased flex flex-col min-h-screen bg-light text-dark dark:bg-dark dark:text-light">
+    <body 
+        class="font-sans antialiased flex flex-col min-h-screen bg-brand-bg text-brand-text">
 
         <x-layout.navbar />
 
